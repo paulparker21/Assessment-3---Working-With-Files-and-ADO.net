9d30d707c8fafc6a2bd1862460727dffd75e94d8
paulparker21 <66097117+paulparker21@users.noreply.github.com>
2020-12-19 01:00:57 -0700

    Add files via upload

---
+++ Assessment 3 - Working With Files and ADO.net/Assessment 3 - Working With Files and ADO.net/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_3___Working_With_Files_and_ADO.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inFile = "../../students.txt"; //the file placed here is the input file 
+            var outFile = Directory.GetCurrentDirectory() + "\\" + "finalGrade"; // This goes to the bin/debug folder in the assingment file
+
+            StreamReader streamReader = new StreamReader(inFile);
+            var line = "";
+            var outf = "";
+            string allTests = "";
+            while ((line = streamReader.ReadLine())!= null) 
+            {
+                Console.WriteLine(line);
+                var values = line.Split('|');
+                int testVal1 = int.Parse(values[1]);
+                int testVal2 = int.Parse(values[2]);
+                int testVal3 = int.Parse(values[3]);
+                int averageVal = (testVal1 + testVal2 + testVal3) / 3;
+                allTests = allTests + values[0] + "|" + averageVal.ToString() + "\n";
+
+                foreach (string v in values)
+                {
+                    Console.WriteLine(v);
+                    outf += v + "\n";
+                
+                }
+            }
+            streamReader.Close();
+
+            var values2 = allTests.Split('\n');
+            for (int i = 0; i < values2.Length-1; i++)
+            {
+                StreamWriter streamWriter = new StreamWriter(outFile + i + ".txt");
+                streamWriter.Write(values2[i]);
+                streamWriter.Close();
+
+            }
+        }
+    }
+}
